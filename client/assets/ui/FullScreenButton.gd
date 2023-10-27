222198601cfe0939f6bb52bf1b15231046576eb8
Christoffer Adolphsson <christoffer.adolphsson@eduro.se>
2023-10-27 02:44:17 +0200

    Added fullscreen icons

--- client/assets/ui/FullScreenButton.gd
+++ client/assets/ui/FullScreenButton.gd
@@ -1,20 +1,20 @@
 extends Button
 
-const EDITOR_EXPAND_ICON := preload("res://icon.png")
-const EDITOR_COLLAPSE_ICON := preload("res://icon.png")
+const EDITOR_EXPAND_ICON := preload("res://sprites/ui/fullscreen-on.png")
+const EDITOR_COLLAPSE_ICON := preload("res://sprites/ui/fullscreen-off.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if OS.has_feature("JavaScript"):
+	#if OS.has_feature("JavaScript"):
 		# Fullscreen does not work well in various browsers, so we use our own JS
 		# code on the browser's side
-		icon = null
-		tooltip_text = ""
-		disabled = true
-		mouse_default_cursor_shape = Control.CURSOR_ARROW
-		custom_minimum_size = Vector2(custom_minimum_size.x, custom_minimum_size.y)
-		focus_mode = Control.FOCUS_NONE
-		return
+	#	icon = null
+	#	tooltip_text = ""
+	#	disabled = true
+	#	mouse_default_cursor_shape = Control.CURSOR_ARROW
+	#	custom_minimum_size = Vector2(custom_minimum_size.x, custom_minimum_size.y)
+	#	focus_mode = Control.FOCUS_NONE
+	#	return
 	
 	connect("pressed", _on_pressed)
 	GlobalSignals.connect("FULLSCREEN_TOGGLED", _update_icon)
@@ -24,6 +24,7 @@ func _ready():
 func _on_pressed() -> void:
 	GlobalValues.is_fullscreen = not GlobalValues.is_fullscreen
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if GlobalValues.is_fullscreen else DisplayServer.WINDOW_MODE_WINDOWED)
+	_update_icon()
 
 
 func _update_icon() -> void:
