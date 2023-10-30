4d0f20a2a60603009c9d31bcff967200a97d39ce
Christoffer Adolphsson <christoffer.adolphsson@eduro.se>
2023-10-30 21:07:57 +0100

    Changed fullscreen icons to 16px. Hopefully not too big but not too small size.

--- website/game/index.service.worker.js
+++ website/game/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1698694637|712973725";
+const CACHE_VERSION = "1698696297|2373434749";
 const CACHE_PREFIX = "mmo-example-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
