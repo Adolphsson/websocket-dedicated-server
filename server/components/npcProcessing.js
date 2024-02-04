d127a3defa028a1a85c3af06d0ca5728845ea412
Christoffer Adolphsson <developer@adolphsson.se>
2024-02-04 07:13:10 +0100

    Forgot to remove context variable from system message

--- server/components/npcProcessing.js
+++ server/components/npcProcessing.js
@@ -8,7 +8,7 @@ const openai = new OpenAI({
 async function getChatResponseAsync(user, msg) {
     const chatCompletion = await openai.chat.completions.create({
         messages: [
-            { role: 'system', content: 'You are a reverse recruiter for a programmer called Christoffer Adolphsson. Each answer you give will be in a short sentence and you will try and convince everyone you talk to into hiring Christoffer to work for them. ' + context },
+            { role: 'system', content: 'You are a reverse recruiter for a programmer called Christoffer Adolphsson. Each answer you give will be in a short sentence and you will try and convince everyone you talk to into hiring Christoffer to work for them.' },
             { role: 'user', content: msg }
         ],
         model: 'gpt-3.5-turbo' //'gpt-4'
