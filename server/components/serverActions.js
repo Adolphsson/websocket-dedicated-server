33c2aefa454f699b21de82dcfb599656578da2be
Christoffer Adolphsson <quarantine@adolphsson.se>
2024-02-24 17:31:18 +0100

    Use unified ProtoMessage function for responses

--- server/components/serverActions.js
+++ server/components/serverActions.js
@@ -8,14 +8,14 @@ const { uuidToUsername } = require('./dataHandler');
 function readyPlayer(wss, ws, parsed, clients) {
     for (let id in uuidToUsername){
         if (parsed.data.username == uuidToUsername[id]){
-            ws.send(JSON.stringify({action:'broadcast', message: 'Already connected...'}));
+            ws.send(ProtoMessage(CMD.PING.id, 0, {message: 'Already connected...'}));
             ws.close()
             return
         }
     }
     uuidToUsername[ws.playerUUID] = parsed.data.username;
     const data = {userData: loadPlayerData(parsed.data.username), playerUUID: ws.playerUUID, playerID: ws.peerID};
-    if (data) ws.send(JSON.stringify({action:'broadcast',data:{function:'load_player_state',parameters:data}}));
+    if (data) ws.send(ProtoMessage(CMD.BROADCAST.id, 0, {function:'load_player_state',parameters:data}));
 }
 
 //This function will receive the player's current state, such as position, animation and etc, and it will send to all the other players online via updatePlayerState().
@@ -37,7 +37,7 @@ function broadcast(wss, ws, parsed, clients){
             // A chat message is being sent, let's read it and send it to chatGPT for a response
             getChatResponseAsync(parsed.data.parameters.username, parsed.data.parameters.text).then(msg => {
                 wss.clients.forEach(client => {
-                    client.send(JSON.stringify({action:'broadcast', data: {function: 'receive_text', parameters: {username: 'Bengt', text: msg, position: '(' + npcPos[0].toString() + ',' + npcPos[1].toString() + ')', audioIndex: Math.round(Math.random()*6)}}}));
+                    client.send(ProtoMessage(CMD.BROADCAST.id, 0, {action:'broadcast', data: {function: 'receive_text', parameters: {username: 'Bengt', text: msg, position: '(' + npcPos[0].toString() + ',' + npcPos[1].toString() + ')', audioIndex: Math.round(Math.random()*6)}}}));
                 });
             });
         }
@@ -45,13 +45,13 @@ function broadcast(wss, ws, parsed, clients){
 
     //TODO: This kind of broadcast can be very costly, try to make it location dependant and only send the update to players within viewing distance of each other
     wss.clients.forEach(client => {
-        client.send(JSON.stringify({action:'broadcast', data: parsed.data}));
+        client.send(ProtoMessage(CMD.BROADCAST.id, 0, parsed.data));
    });
 };
 
 //This function will respond to the client that send the request and can be used to measure the round trip time.
 function ping(wss, ws, parsed, clients){
-    ws.send(JSON.stringify({ action: 'pong', data: parsed.data }));
+    ws.send(ProtoMessage(CMD.PING.id, 0, parsed.data));
 };
 
 function signaling(wss, ws, parsed, clients) {
