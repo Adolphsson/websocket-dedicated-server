fbda5fa101137c78d5d7eea1bf3a1294bbf47eda
Christoffer Adolphsson <quarantine@adolphsson.se>
2024-02-26 03:20:43 +0100

    Activated voip test

--- website/game/index.service.worker.js
+++ website/game/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1708911487|17329283195";
+const CACHE_VERSION = "1708913975|19817152728";
 const CACHE_PREFIX = "island-demo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
