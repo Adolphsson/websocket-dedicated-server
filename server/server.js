088e872618ddb836dedbf209ad40bdc74e0a3de5
Christoffer Adolphsson <quarantine@adolphsson.se>
2024-02-26 02:01:36 +0100

    Added a more descriptive reason for closing connections

--- server/server.js
+++ server/server.js
@@ -266,6 +266,45 @@ function signalHandling(wss, peer, json) {
     throw new ProtoError(4000, STR_INVALID_CMD);
 }
 
+let specificStatusCodeMappings = {
+    '1000': 'Normal Closure',
+    '1001': 'Going Away',
+    '1002': 'Protocol Error',
+    '1003': 'Unsupported Data',
+    '1004': '(For future)',
+    '1005': 'No Status Received',
+    '1006': 'Abnormal Closure',
+    '1007': 'Invalid frame payload data',
+    '1008': 'Policy Violation',
+    '1009': 'Message too big',
+    '1010': 'Missing Extension',
+    '1011': 'Internal Error',
+    '1012': 'Service Restart',
+    '1013': 'Try Again Later',
+    '1014': 'Bad Gateway',
+    '1015': 'TLS Handshake'
+};
+
+function getStatusCodeString(code) {
+    if (code >= 0 && code <= 999) {
+        return '(Unused)';
+    } else if (code >= 1016) {
+        if (code <= 1999) {
+            return '(For WebSocket standard)';
+        } else if (code <= 2999) {
+            return '(For WebSocket extensions)';
+        } else if (code <= 3999) {
+            return '(For libraries and frameworks)';
+        } else if (code <= 4999) {
+            return '(For applications)';
+        }
+    }
+    if (typeof(specificStatusCodeMappings[code]) !== 'undefined') {
+        return specificStatusCodeMappings[code];
+    }
+    return '(Unknown)';
+}
+
 wss.on('connection', (ws, req) => {
     if (Object.keys(clients).length >= MAX_PEERS) {
 		ws.close(4000, STR_TOO_MANY_PEERS);
@@ -331,7 +370,7 @@ wss.on('connection', (ws, req) => {
     ws.on('close', (code, reason) => {
         peersCount--;
         console.log(`Connection with peer ${peer.id} closed `
-			+ `with reason ${code}: ${reason}`);
+			+ `with reason ${code} - ${getStatusCodeString(code)}: ${reason}`);
 		if (peer.lobby && lobbies.has(peer.lobby)
 			&& lobbies.get(peer.lobby).leave(peer)) {
 			lobbies.delete(peer.lobby);
